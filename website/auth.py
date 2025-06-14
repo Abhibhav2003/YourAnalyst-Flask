495be5291e6d4171ed8c62c26266507118d077ea
Abhibhav Sharma <139039304+Abhibhav2003@users.noreply.github.com>
2025-06-14 22:34:18 +0530

    implement authentication

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,71 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,flash,redirect,url_for
+from .models import User
+from . import db
+from flask_login import login_user,login_required,logout_user,current_user
+from werkzeug.security import generate_password_hash,check_password_hash
 
 auth = Blueprint('auth',__name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
+   if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        remember = request.form.get('remember')
+        user = User.query.filter_by(email=email).first()
 
-@auth.route('/signup')
+        if user:
+            if user.username != username:
+                flash('Incorrect Username', category='error')
+            elif check_password_hash(user.password, password):
+                flash('Logged In Successfully', category='success')
+                if remember:
+                  login_user(user,remember=True)
+                else:
+                   login_user(user,remember=False)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect Password', category='error')
+        else:
+            flash('No account found with that email', category='error')
+
+   return render_template('login.html')
+ 
+
+@auth.route('/signup',methods=["GET","POST"])
 def signup():
-    return render_template('signup.html')
+   if request.method == 'POST':
+       username = request.form['username']
+       password = request.form['password']
+       confirm_password = request.form['confirm_password']
+       email = request.form['email']
+       
+       user = User.query.filter_by(email=email).first()
+
+       if user:
+          flash('Email already exists',category='error')
+       if len(email) < 4:
+          flash("Email must be greater than 4 characters",category="error")
+       elif len(username) < 3:
+          flash("Username must be greater than or equal to 3 characters",category="error")
+       elif password != confirm_password:
+          flash("Passwords don't match",category="error")
+       elif len(password) < 8:
+          flash("Password must be of 8 characters",category="error")
+       elif len(password) > 8:
+          flash("Password must be of 8 characters",category="error")
+       else:
+          new_user = User(email=email,username=username,password=generate_password_hash(password,method='pbkdf2:sha256'))
+          db.session.add(new_user)
+          db.session.commit()
+          flash('Account created!',category="success")
+          return redirect(url_for('auth.login'))
+   return render_template('signup.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+   logout_user()
+   return redirect(url_for('views.start'))
--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint,render_template
-
+from flask import Blueprint,render_template,request,redirect
+from flask_login import login_required,current_user
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def start():
     return render_template('start.html')
+
+@login_required
+@views.route('/home')
+def home():
+    return render_template('home.html',message=current_user.username)
