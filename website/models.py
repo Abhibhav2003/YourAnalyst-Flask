7e2ff5f729d238a20b0a812f9875e09a16153be2
Abhibhav Sharma <139039304+Abhibhav2003@users.noreply.github.com>
2025-07-19 20:10:05 +0530

    Add docstring in models file

--- website/models.py
+++ website/models.py
@@ -1,8 +1,10 @@
 from . import db
-from flask_login import UserMixin
+from flask_login import UserMixin 
 
 class User(db.Model,UserMixin):
+    '''This is a User model that stores information about the user, such as 
+       id, email, hashed password and username'''
     id = db.Column(db.Integer,primary_key=True)
     email = db.Column(db.String(150),unique=True,nullable=False) 
-    password = db.Column(db.String(256), nullable=False)
+    password = db.Column(db.String(256), unique=True,nullable=False)
     username = db.Column(db.String(20),unique=True,nullable=False)
